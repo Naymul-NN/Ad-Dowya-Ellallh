f53b89aedffaf2fbf96605c2cd398b0bf8a84fd9
Naymul-NN <naymulislam108@gmail.com>
2024-05-10 13:45:47 +0600

    new title

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div>
-      <h1 className='text-4xl text-green-900'>love you</h1>
+      <h1 className='text-4xl text-green-500 font-bold text-center pt-4'>do the job by your own knowledge love you</h1>
     </div>
   );
 };
